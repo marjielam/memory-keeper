445363d5bc135caa7dec7f88bcac6e623c8a609f
Marjie Lam <marjielam@gmail.com>
2017-01-31 13:26:39 -0500

    added answer functionality

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org/'
 
 gem 'devise'
+gem 'font-awesome-rails'
 gem 'foundation-rails'
 gem 'jquery-rails'
 gem 'pg', '~> 0.18'
--- react/src/main.js
+++ react/src/main.js
@@ -6,8 +6,18 @@ import DayIndex from './components/DayIndex';
 
 $(function() {
   if (document.getElementById('day-show')) {
+    let dayId = parseInt(document.getElementById('day-show').dataset.id);
+    let questionId = parseInt(document.getElementById('question-id').value);
+    let questionBody = document.getElementById('question-body').value;
+    let currentUserId = parseInt(document.getElementById('current-user-id').value);
     ReactDOM.render(
-      <Day />,
+      <Day
+      key={dayId}
+      dayId={dayId}
+      questionId={questionId}
+      questionBody={questionBody}
+      currentUserId={currentUserId}
+      />,
       document.getElementById('day-show')
     );
   } else if (document.getElementById('day-index')) {
